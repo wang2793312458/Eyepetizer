dcc0a9f3f2f6799328dfcfbd71ac6c09133a2746
wz2793312458 <wz2793312458@163.com>
2017-08-11 18:20:21 +0800

    Initial commit

---
+++ app/src/main/java/com/tt/lvruheng/eyepetizer/mvp/presenter/FindPresenter.kt
@@ -0,0 +1,35 @@
+package com.tt.lvruheng.eyepetizer.mvp.presenter
+
+import android.content.Context
+import com.tt.lvruheng.eyepetizer.mvp.contract.FindContract
+import com.tt.lvruheng.eyepetizer.mvp.model.FindModel
+import com.tt.lvruheng.eyepetizer.mvp.model.bean.FindBean
+import com.tt.lvruheng.eyepetizer.utils.applySchedulers
+import io.reactivex.Observable
+
+/**
+ * Created by lvruheng on 2017/7/6.
+ */
+class FindPresenter(context: Context, view: FindContract.View) : FindContract.Presenter {
+    var mContext: Context? = null
+    var mView: FindContract.View? = null
+    val mModel: FindModel by lazy {
+        FindModel()
+    }
+
+    init {
+        mView = view
+        mContext = context
+    }
+
+    override fun start() {
+        requestData()
+    }
+
+    override fun requestData() {
+        val observable: Observable<MutableList<FindBean>>? = mContext?.let { mModel.loadData(mContext!!) }
+        observable?.applySchedulers()?.subscribe { beans: MutableList<FindBean> ->
+            mView?.setData(beans)
+        }
+    }
+}
